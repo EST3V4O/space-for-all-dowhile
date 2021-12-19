8feb1f68097c98fa202f10ad69fc00f4f3875d93
Estevão Gabriel Biondi <teh.gabriel10@gmail.com>
2021-12-18 23:53:48 -0300

    active link by section

--- src/components/MenuLink/index.tsx
+++ src/components/MenuLink/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import { ReactNode } from "react";
+import { useRouter } from "next/router";
+import { ReactNode, useCallback, useEffect, useState } from "react";
 
 import styles from './styles.module.scss'
 
@@ -11,13 +12,54 @@ interface MenuLinkProps {
 }
 
 export function MenuLink({ handleClick, href, children, labelForTitle }: MenuLinkProps) {
+  const [active, setActive] = useState(false)
+
+  const handleLinkActive = useCallback(() => {
+    handleClick(labelForTitle)
+  }, [handleClick, labelForTitle])
+
+  useEffect(() => {
+    const link = href.split("#")[1]
+    const unregister = () =>  {
+      const { scrollHeight, offsetTop } = document.querySelector<HTMLElement>(`section[id='${link}']`)
+
+      const discountMenu = offsetTop - 85
+      const elementHeightScroll = discountMenu + scrollHeight
+
+      if(window.scrollY >= discountMenu && window.scrollY <= elementHeightScroll) {
+        if(!active) {
+          setActive(true)
+          handleLinkActive()
+        }
+      } else {
+        setActive(false)
+      }
+
+    }
+    window.addEventListener("scroll", unregister)
+
+    return () => window.removeEventListener("scroll", unregister)
+  }, [href, active, handleLinkActive])
+
+  useEffect(() => {
+    if(!window.location.href.includes("#") && href === "#home") {
+      setActive(true)
+      window.location.href = href
+      return 
+    }
+
+    if(window.location.href === href) {
+      setActive(true)
+    }
+  }, [href])
+
   return (
     <li
       onClick={() => handleClick(labelForTitle)}
       className={styles.container}
     >
       <Link href={href}>
-        <a>{children}</a>
+        <a className={active ? styles.active : ""} >{children}</a>
       </Link>
     </li>
   )
